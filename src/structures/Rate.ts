b6aee5e845374f562c24c7859edab54da0924a47
Dann <46873266+Aeol-ian@users.noreply.github.com>
2022-03-31 14:21:52 +0400

    Add files via upload

--- src/structures/Rate.ts
+++ src/structures/Rate.ts
@@ -5,6 +5,7 @@ import { GameModel } from "../database/models/GameModel";
 import { ServerModel } from "../database/models/ServerModel";
 import { Currency, CurrencyType } from "../docs/currency/Main";
 import { TextExp } from "../docs/languages/createText";
+import { findLevel } from "../docs/levels/levels";
 import { Embed } from "./Embed";
 import { Functions } from "./Functions";
 import { Page } from "./InterfaceEdition";
@@ -63,6 +64,16 @@ export class Rate {
                             return await a.RepTop();
                         },
                         nextPage: true
+                    },
+                    {
+                        button: new MessageButton()
+                            .setCustomId("XP_TOP")
+                            .setStyle("SECONDARY")
+                            .setEmoji("⭐"),
+                        async action() {
+                            return await a.XpTop();
+                        },
+                        nextPage: true
                     }
                 ]
             }
@@ -117,6 +128,31 @@ export class Rate {
             
         }
 
+    }
+
+    private async XpTop(): Promise<Page<MessageEmbed>> {
+        const a = this;
+        return {
+            async embed() {
+                const all = await models.users.find({xp: {$exists: true}}).exec();
+                const filtered = all.filter(data => data.xp);
+                const sorted = filtered.sort((a, b) => b.xp - a.xp);
+                const sliced = sorted.slice(0, 10);
+                const texted = await Promise.all(sliced.map(async (data, i) => {
+                    const user = await a.client.users.fetch(data._id);
+                    const name = user ? user.tag : a.t(57);
+
+                    return `**${i + 1}.** ${name} — \`${a.client.util.formatNumber(Math.round(data.xp || 0))} xp\` [⭐ **${a.client.util.formatNumber(findLevel(data.xp || 0).currentLevel)}**]`
+                }));
+                return Embed(a.msg).setText(`🏆 | ${a.t(58)} ⭐\n\n${texted?.length > 0 ? texted.join("\n") : a.t(56)}`) as MessageEmbed;
+            },
+            customId: "a",
+            description: "a",
+            buttonLabel: "a",
+            buttonType: "SECONDARY",
+            type: "Group"
+            
+        }
     }
 
     async getData() {
